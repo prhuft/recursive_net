bd7ec25b4bc5afacff09e7aa08c8f46a1333d07a
Preston Huft <gothrowayo@gmail.com>
2017-12-28 17:43:25 -0600

    Syntactical errors fixed. ''activations'' may not be the correct dimensions, as activations[-1] returns a 2D list rather than a 1D list of same length as the output, as expected.

--- recursive1.py
+++ recursive1.py
@@ -3,7 +3,7 @@ Incentivized neural network for autonomous learning.
 
 Preston Huft, December 2017
 
-Teach a point to walk down a hill --> very simple, I hope.
+Autonomous learning to plot points (x,y) where y = x^2. --> simple, I hope.
 
 Qualitative description of how it should work: 
 
@@ -27,7 +27,8 @@ from matplotlib import pyplot as plt
 class Cost(object):
 
 	@staticmethod
-	def ce(a, y):
+	def ce(a, y): # Note: this is never actually called, as it turns out we only
+	# need its derivative wrt to the correct output, i.e. delta. 
 		"""Returns the cross entropy cost function for an ouptut ``a`` and a
 		desired output ``y``. np.nan_to_num forces infinite values to be large 
 		finite values of the appropriate sign. """
@@ -37,7 +38,7 @@ class Cost(object):
 	@staticmethod
 	def delta(a, y):
 		"""Returns the error delta for the output layer L."""
-	
+		print("a:, y:",a,y)
 		return (a-y)
 
 #### The output criterion
@@ -72,7 +73,7 @@ class Criterion(object):
 		is far off, i.e. ``kappa`` near 0, a_L will be mostly random."""
 		
 		# Note that a_L is in [0,1]
-		a_L = .5*((1-kappa)*a_r(len(a_L)) + kappa*a_L)
+		a_L = .5*((1-kappa)*Criterion.a_r(len(a_L)) + kappa*a_L)
 		return a_L
 	
 	@staticmethod
@@ -82,10 +83,12 @@ class Criterion(object):
 		
 		return np.random.sample(output_size)
 		
+	@staticmethod	
 	def new_input(output):
-		"""For this simple net, the raw output is the new input."""
-	
-		return output
+		"""For this simple net, the rounded output is the new input."""
+		input = int(output + .5)
+		
+		return input
 
 #### The neural network 
 class Network(object):
@@ -103,13 +106,15 @@ class Network(object):
 		
 		self.num_layers = len(sizes)
 		self.sizes = sizes
-		self.a_L = a_r(sizes[-1])
+		self.cost = Cost
+		self.a_L = Criterion.a_r(sizes[-1])
+		self.weight_initializer()
 		
-		x = b10_to_b2(input[0],bits)
-		y = b10_to_b2(input[0],bits)
+		self.x0 = b10_to_b2(input[0],self.bits)
+		self.y0 = b10_to_b2(input[1],self.bits)
 		
-		self.last_input = np.append(x,y)
-		self.new_input = zeros(2*bits)
+		self.last_input = np.append(self.x0,self.y0)
+		self.new_input = np.zeros(2*self.bits)
 		self.kappa = 0 # The network standard will start out being random
 		self.kappas = []
 		
@@ -126,7 +131,7 @@ class Network(object):
 		
 		# no. of rows = no. of neurons in layer l+1, 
 		# no. of cols = no. of neurons in layer l
-		self.weights = [np.random(y,x) for x,y in zip(self.sizes[:-1],
+		self.weights = [np.random.randn(y,x) for x,y in zip(self.sizes[:-1],
 						self.sizes[1:])] 
 	
 	def feedforward(self, a):
@@ -146,74 +151,85 @@ class Network(object):
 		times to cycle the network during a single epoch."""
 		
 		# Loop through the training epochs
-		for epoch in epochs:
+		for m in range(0, epochs):
 			for n in range(0, epoch_size):
 			
-				self.kappas = []
-
+				self.kappas = []				
+				# Start the epoch at the initial (x,y) input
+				input0 = np.append(self.x0, self.y0)
+				#print("input0: ", input0)
+				
 				# Update the weights and biases after running a single input
-				self.update(epoch_size, eta, lmbda, epoch)
+				self.update(input0, epoch_size, eta, lmbda, epochs)
 				
 				# How'd we do? 
 				plot_kappa(self.kappas)
 			
-			
-			
-	def update(self, epoch_size, eta, lmbda, epochs):
-		    """Update the network's weights and biases by applying gradient
+	def update(self, init_input, epoch_size, eta, lmbda, epochs):
+		"""Update the network's weights and biases by applying gradient
         descent using backpropagation over a single epoch, i.e. ``epoch_size``
-		tries to succeed. Recall there is no training data; subsequent inputs
+		iterations. Recall there is no training data; subsequent inputs
 		are born of previous outputs. ``eta`` is the learning rate, ``lmbda`` is
-		the regularization parameter, and ``epochs`` is the total number of 
-		epochs. """
+		the regularization parameter, ``epochs`` is the total number of epochs,
+		and ``init_input`` is the list of inputs to the input layer."""
 		
-		# n = epochs*epoch_size
-		# nabla_b = [np.zeros(b.shape) for b in self.biases]
-		# nabla_w = [np.zeros(w.shape) for w in self.weights]
+		n = epochs*epoch_size
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+		
+		input = init_input
 		
 		for i in range(0, epoch_size):
 		
 			# The current input and output standard. Note that self.a_L 
 			# and self.kappa are updated in self.backprop
-			x = Criterion.new_input(self.a_L)
-			y = Criterion.a_L(self.kappa, self.a_L)
 			
-			delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+			output_standard = Criterion.a_L(self.kappa, self.a_L)
+			#print("output_std: ", output_standard)
+			
+			delta_nabla_b, delta_nabla_w = self.backprop(input, output_standard)
 			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
 			nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
 			
-        self.weights = [(1-eta*(lmbda/n))*w-(eta/epoch_size)*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/epoch_size)*nb
-                       for b, nb in zip(self.biases, nabla_b)]		
+			# Update the input
+			input = Criterion.new_input(self.a_L)
+			
+        # self.weights = [(1-eta*(lmbda/n))*w-(eta/epoch_size)*nw
+                        # for w, nw in zip(self.weights, nabla_w)]
+        # self.biases = [b-(eta/epoch_size)*nb
+                       # for b, nb in zip(self.biases, nabla_b)]		
 		
 	def backprop(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+		"""Return a tuple ``(nabla_b, nabla_w)`` representing the
+		gradient for the cost function C_x.  ``nabla_b`` and
+		``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+		to ``self.biases`` and ``self.weights``."""
+		
+		print("input: ", x)
+		print("output_std", y)
+		
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
-        activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the input vectors, layer by layer
-        for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
-            zs.append(z)
-            activation = sigmoid(z)
-            activations.append(activation)
+		activation = x
+		activations = [activation] # list to store all the activations, layer by layer
+		zs = [] # list to store all the input vectors, layer by layer
+		for b, w in zip(self.biases, self.weights):
+			z = np.dot(w, activation)+b
+			zs.append(z)
+			activation = sigmoid(z)
+			activations.append(activation)
         # backward pass
-        delta = (self.cost).delta(zs[-1], activations[-1], y)
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-       
-        for l in xrange(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+		delta = (self.cost).delta(activations[-1], y.transpose())
+		nabla_b[-1] = delta
+		nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+		
+		for l in xrange(2, self.num_layers):
+			z = zs[-l]
+			sp = sigmoid_prime(z)
+			delta = np.dot(self.weights[-l+1].transpose(), delta)*sp
+			nabla_b[-l] = delta
+			nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
 			
 		# Store the last activation layer and compute kappa
 		self.a_L = activations[-1]
@@ -224,8 +240,7 @@ class Network(object):
 		# Why is last_input == new_input? Self.new input is updated before 
 		# we compare the two, so this is fine.
 		self.last_input = self.new_input
-		
-        return (nabla_b, nabla_w)
+		return (nabla_b, nabla_w)
 
 #### Miscellaneous functions 
 	
@@ -255,23 +270,23 @@ def b10_to_b2(number, bits):
 			
 	return result
 	
-def input_to_num(x): # Why this not working????
-	"""Converts input value array to base 10 float with one decimal point. 
-	For example, [.8, .3, .1, .6] would become 831.6. The last value in the
-	array is assumed to be the tenths place."""
+# def input_to_num(x): # Why this not working????
+	# """Converts input value array to base 10 float with one decimal point. 
+	# For example, [.8, .3, .1, .6] would become 831.6. The last value in the
+	# array is assumed to be the tenths place."""
 	
-	result = 0
-	max = len(x)-1
-	for n in range(max,0):
-		result += x[n]*np.power(10,max-n)
+	# result = 0
+	# max = len(x)-1
+	# for n in range(max,0):
+		# result += x[n]*np.power(10,max-n)
 	
-	return result
+	# return result
 	
-def input_to_xy(input)
+def input_to_xy(input):
 	"""Convert the neural input/output list to an (x,y) pair."""
 	
 	# Round to 0 or 1
-	for i in range(0,len(input))
+	for i in range(0,len(input)):
 		input[i] = int(input[i]+0.5)
 		
 	cols = len(input)
@@ -295,17 +310,21 @@ def plot_inputs(inputs):
 	
 	x = np.zeros(rows)
 	y = np.zeros(rows)
-	for i in range(0, rows)
+	for i in range(0, rows):
 		x[i], y[i] = input_to_xy(input_arr[i])
 		
 	p = plt.plot(x,y)
 	plt.show(p)
-		
-	
-		
-		
-	
-		
-	
 
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
 	
+#### Implement some test code
+
+net = Network([16,10,16],[0,0])
+net.SGD(10,10,100)
